4efa7eed4211917440d9e60b51c8f07af6818ce6
neko <techno.cat.miau@gmail.com>
2021-10-19 03:49:04 +0900

    木戸泉を追加

---
+++ favorite/src/kidoizumi.pl
@@ -0,0 +1,21 @@
+{
+    brewery => {
+        name => '木戸泉酒造株式会社',
+        kana => 'きどいずみしゅぞう株式会社',
+        prefecture => '千葉県'
+    },
+    brand => {
+        name => '木戸泉',
+        kana => 'きどいずみ',
+        url => 'http://www.kidoizumi.jp/'
+    },
+    products => [
+        {
+            name => '木戸泉 純米 秋あがり',
+            comment => "まずはミルクチョコレートの香り。
+酸味が特徴的で食事に合う。唐揚げにも合う。",
+            memo => 'todo: ぬる燗をためす',
+            updated_date => '2021/10/19'
+        }
+    ]
+};
